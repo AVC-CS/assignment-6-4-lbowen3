8062942ac36a10c781efda08d6bebf9872b00439
lbowen3 <144710267+lbowen3@users.noreply.github.com>
2024-03-15 19:06:49 +0000

    Edited Code

--- main.hpp
+++ main.hpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void swapValues(int &n1, int &n2);
-void swapValues(int &n1, int &n2, int &n3);
-void swapValues(int &n1, int &n2, int &n3, int &n4);
+void swapValues(int &, int &);
+void swapValues(int &, int &, int &);
+void swapValues(int &, int &, int &, int &);
 
 
-void swampValues(int &n1, int &n2)
+void swapValues(int &n1, int &n2)
 {
+    int tmp;
+    tmp = n1;
     n1 = n2;
-    n2 = n1;
-    return;
+    n2 = tmp;
 }
-void swampValues(int &n1, int &n2, int &n3)
+void swapValues(int &n1, int &n2, int &n3)
 {
+    int tmp;
+    tmp = n1;
     n1 = n2;
     n2 = n3;
-    n3 = n1;
-    return;
+    n3 = tmp;
 }
-void swampValues(int &n1, int &n2, int &n3, int &n4)
+
+void swapValues(int &n1, int &n2, int &n3, int &n4)
 {
+    int tmp1, tmp2;
+    tmp1 = n2;
+    tmp2 = n1;
     n1 = n4;
     n2 = n3;
-    n3 = n2;
-    n4 = n1;
-    return;
+    n3 = tmp1;
+    n4 = tmp2;
 }
